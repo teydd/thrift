3c782b60d2348f644a9e1aa71c0c698fef7b4226
teydd <chemosted@gmail.com>
2025-12-04 22:55:17 +0300

    product detail page

---
+++ components/product-detail.tsx
@@ -0,0 +1,61 @@
+"use client"
+
+import Image from "next/image";
+import Stripe from "stripe";
+import { Button } from "./ui/button";
+import { useCartStore } from "@/store/cart-store";
+
+interface Props {
+  product: Stripe.Product;
+}
+
+export const ProductDetail = ({ product }: Props) => {
+  const { items, addItem, removeItem } = useCartStore()
+  const price = product.default_price as Stripe.Price;
+  const cartItem = items.find((item)=> item.id === product.id)
+  const quantity = cartItem ? cartItem.quantity : 0
+  const onAddItem = ()=>{
+    addItem({
+      id:product.id,
+      name:product.name,
+      price:price.unit_amount as number,
+      imageUrl:product.images ? product.images[0] : null,
+      quantity:1
+    })
+  }
+  return (
+    <>
+      <div className="container ms-auto px-4 py-8 flex flex-col md:flex-row gap-8 items-center space-x-30 shadow-lg">
+        {product.images && product.images[0] && (
+          <div className="relative h-96 md:w-1/2 rounded-lg overflow-hidden">
+            <Image
+              src={product.images[0]}
+              alt={product.name}
+              layout="fill"
+              objectFit="cover"
+              className="hover:opacity-90 transition duration-300"
+            />
+          </div>
+        )}
+
+        <div className="md:w-1/2">
+          <h1 className="text-3xl font-bold mb-4">{product.name}</h1>
+          {product.description && (
+            <p className="text-gray-700 mb-4"> {product.description} </p>
+          )}
+          {price && price.unit_amount && (
+            <p className="text-lg font-semibold text-gray-900">
+              {" "}
+              KES {(price.unit_amount / 100).toFixed(2)}{" "}
+            </p>
+          )}
+          <div className=" flex items-center space-x-4">
+            <Button onClick={()=> removeItem(product.id)} variant="outline">-</Button>
+            <span className="text-lg font-semibold"> {quantity}</span>
+            <Button onClick={onAddItem} variant="outline">+</Button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
