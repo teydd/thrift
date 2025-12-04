c96efb298dd36fa11e620d58a4bb72e96fce8751
teydd <chemosted@gmail.com>
2025-12-04 22:50:10 +0300

    home page update

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,31 @@
+import { Carousel } from "@/components/carousel";
+import { Button } from "@/components/ui/button";
 import { stripe } from "@/lib/stripe";
 import Image from "next/image";
+import Link from "next/link";
 
 export default async function Home() {
   const products = await stripe.products.list({
     expand:["data.default_price"],
     limit: 3,
   })
-  console.log(products)
   return (
     <>
     <div>
-      <section>
-        <div>
-          <div>
-            <h2>Welcome to the Thrift Store</h2>
-            <p>Discover the latest products at the best prices</p>
+      <section className="rounded bg-neutral-100 py-8 sm:py-12">
+        <div className="mx-auto grid grid-cols-2 items-center justify-items-center gap-8 px-8 sm:px-16">
+          <div className="max-w-md space-y-4">
+            <h2 className="text-3xl fonst-bold tracking-tight md:text-4xl">Welcome to the Thrift Store</h2>
+            <p className="text-neutral-600">Discover the latest products at the best prices</p>
+            <Button className="inline-flex items-center justify-center rounded-full px-6 py-3 bg-black text-white" asChild variant={"default"}>
+              <Link className="inline-flex items-center justify-center rounded-full px-6 py-3 bg-black text-white" href={"/shop"}>Browse All Products</Link>
+            </Button>
           </div>
+          <Image className="rounded" alt="Banner Image" width={450} height={450} src={products.data[0].images[0]}></Image>
         </div>
+      </section>
+      <section className="py-8">
+      <Carousel products={products.data}/>
       </section>      
     </div>    
     </>
