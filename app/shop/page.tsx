c1d5d6998268633051b52aaebc9db79ab305b8ff
teydd <chemosted@gmail.com>
2025-12-04 22:51:40 +0300

    shop showing products update

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,7 +1,20 @@
+
+import { ProductList } from '@/components/product-list'
+import { stripe } from '@/lib/stripe'
 import React from 'react'
 
-export default function Shop() {
+export default async function Shop() {
+  const products = await stripe.products.list({
+      expand:["data.default_price"],
+    })
   return (
-    <div>Shop</div>
+    <>
+    <div className='pb-8'>
+      <h1 className='text-4xl text-center font-bold hover:text-cyan-400 tracking-tight mb-8'>
+      Welcome to the Shop
+    </h1>
+    <ProductList products = {products.data} />
+    </div>
+    </>
   )
 }
