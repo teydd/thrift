eddd33d69e3831b91a5ffe13195308d260ecce80
teydd <chemosted@gmail.com>
2025-12-05 01:18:18 +0300

    prettier

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,8 @@ export default function Footer() {
   return (
     <footer className="bg-gray-100 shadow-t-lg py-4 mt-8">
       <div className="container mx-auto text-center text-sm text-gray-600">
-        &copy; {new Date().getFullYear()} Thrift Store by Teddy Chemos. All rights reserved.
+        &copy; {new Date().getFullYear()} Thrift Store by Teddy Chemos. All
+        rights reserved.
       </div>
     </footer>
   );
