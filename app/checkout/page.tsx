3e5412c4021413bc636fd1101a889de29b33fd80
teydd <chemosted@gmail.com>
2025-12-04 22:51:10 +0300

    checkout page

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,7 +1,56 @@
+"use client"
+
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { useCartStore } from '@/store/cart-store'
 import React from 'react'
+import { checkoutAction } from './checkout-action'
 
 export default function Checkout() {
+  const { items, removeItem, addItem, clearCart } = useCartStore()
+  const total = items.reduce((acc,item)=> acc + item.price * item.quantity, 0)
+
+  if(total === 0 || items.length === 0){
+    return(
+      <div>
+        <h1>Your Cart is Empty</h1>
+      </div>
+    )
+  }
   return (
-    <div>Checkout</div>
+    <>
+    <div className='container mx-auto px-4 py-8'>
+      <h1 className='text-3xl font-bold mb-8 text-center'>Checkout</h1>
+      <Card className='max-w-md mx-auto mb-8'>
+        <CardHeader>
+          <CardTitle className='text-xl font-bold'>Order Summary</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <ul className='space-y-4'>
+            {items.map((item,key)=>(
+              <li key={key} className='flex flex-col gap-2 border-b pb-2'>
+                <div className='flex justify-between'>
+                  <span className='font-medium'> { item.name }</span>
+                  <span className='font-semibld'> KES {(item.price * item.quantity / 100).toFixed(2)} </span>
+                </div>
+                <div className='flex items-center gap-2'>
+                  <Button size="sm" onClick={()=> removeItem(item.id)} variant="outline">-</Button>
+            <span className="text-lg font-semibold"> {item.quantity}</span>
+            <Button onClick={()=> addItem({...item, quantity:1})} variant="outline">+</Button>
+                </div>
+              </li>
+            ))}
+          </ul>
+          <div className='mt-4 border-t pt-2 text-lg font-semibold'>
+            Total : KES { (total/100).toFixed(2)}
+          </div>
+        </CardContent>
+      </Card>
+      <form className='max-w-md mx-auto' action={checkoutAction}>
+        <input type="hidden" name = "items" value={JSON.stringify(items)} />
+        <Button type='submit' className='w-full' variant="default">Procees to Payment</Button>
+      </form>
+    </div>
+    </>
   )
 }
